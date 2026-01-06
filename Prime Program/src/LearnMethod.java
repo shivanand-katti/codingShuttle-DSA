6109c93dcb5deac645e8a5687b8e198abde2a425
shivusoumya <shivadatacamp@gmail.com>
2026-01-06 09:34:40 +0530

    Learn patters

--- Prime Program/src/LearnMethod.java
+++ Prime Program/src/LearnMethod.java
@@ -1,2 +1,71 @@
 public class LearnMethod {
+    public static void main(String[] args) {
+        System.out.println("Inside main method" + args.toString());
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Hello, welcome to LearnMethod class!");
+    }
+
+    // Static methods can be called without creating an instance of the class
+
+    // Instance methods require an object of the class to be created before they can be called
+    // For example:
+    public void instanceGreet() {
+        System.out.println("Hello from the instance method!");
+    }
+
+    // Method with parameters
+    static int add(int a, int b) {
+        return a + b;
+    }
+    // Method with return type
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method overloading
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Recursive method
+    static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    // Variable-length arguments
+    static int sum(int... numbers) {
+        int total = 0;
+        for (int num : numbers) {
+            total += num;
+        }
+        return total;
+    }
+
+    // Main method to demonstrate instance method call
+    public static void demonstrateInstanceMethod() {
+        LearnMethod lm = new LearnMethod();
+        lm.instanceGreet();
+    }
+
+    // Entry point to demonstrate all methods
+    public static void demonstrateAll() {
+        greet();
+        demonstrateInstanceMethod();
+        System.out.println("Addition (2 args): " + add(5, 10));
+        System.out.println("Addition (3 args): " + add(5, 10, 15));
+        System.out.println("Multiplication: " + multiply(5, 10));
+        System.out.println("Factorial of 5: " + factorial(5));
+        System.out.println("Sum of variable args: " + sum(1, 2, 3, 4, 5));
+    }
+
+    // You can uncomment the following line to run all demonstrations
+    // public static void main(String[] args) { demonstrateAll(); }
+
 }
---
+++ Prime Program/src/Patterns.java
@@ -0,0 +1,97 @@
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        // Pattern 1: Right-Angled Triangle
+        System.out.println("Pattern 1: Right-Angled Triangle");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 2: Inverted Right-Angled Triangle
+        System.out.println("\nPattern 2: Inverted Right-Angled Triangle");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 3: Pyramid
+        System.out.println("\nPattern 3: Pyramid");
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Pattern 4: Inverted Pyramid
+        System.out.println("\nPattern 4: Inverted Pyramid");
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Pattern 5: Diamond
+        System.out.println("\nPattern 5: Diamond");
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of Diamond
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Pattern 6: Hollow Square
+        System.out.println("\nPattern 6: Hollow Square");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        // Pattern 7: Hollow Right-Angled Triangle
+        System.out.println("\nPattern 7: Hollow Right-Angled Triangle");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        // End of patterns
+    }
+}
